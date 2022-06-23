8447ea60d1409ee594df9f8eae791c3d1d99342e
raminenieswarrao <65676261+raminenieswarrao@users.noreply.github.com>
2022-06-23 09:23:59 +0530

    Add files via upload

---
+++ jsfile.js
@@ -0,0 +1,17 @@
+console.log('Building First project on Java Script' + \n);
+console.log('Program to test js file');
+// program to display the date
+// get local machine date time
+const date = new Date();
+
+// get the date as a string
+const n = date.toDateString();
+
+// get the time as a string
+const time = date.toLocaleTimeString();
+
+// display date
+console.log('Date: ' + n);
+
+// display time
+console.log('Time: ' + time);
